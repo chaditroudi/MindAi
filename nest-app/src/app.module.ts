70d3ec043833040176d60a7a01aeb6a6edd3df02
chaditroudi <troudishedy6@gmail.com>
2026-06-17 19:22:01 +0200

    Updated Changes

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -34,7 +34,7 @@ const angularBuilt = existsSync(path.join(angularDist, 'index.html'));
     ...(angularBuilt
       ? [ServeStaticModule.forRoot({
           rootPath: angularDist,
-          exclude:  ['/api*', '/health'],
+          exclude:  ['/api{/*path}', '/health'],
         })]
       : []),
   ],
