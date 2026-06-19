6cf8722ca68b38fa0196c5b0a0b5e33aba1b826a
chaditroudi <troudishedy6@gmail.com>
2026-06-19 12:10:14 +0200

    Updated Changes

--- nest-app/src/database/database.module.ts
+++ nest-app/src/database/database.module.ts
@@ -6,10 +6,10 @@ import { MongooseModule } from '@nestjs/mongoose';
   imports: [
     MongooseModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (cfg: ConfigService) => {
-        const uri = cfg.get<string>('mongodb.uri');
-        const dbName = cfg.get<string>('mongodb.db');
-        const serverSelectionTimeoutMS = cfg.get<number>('mongodb.serverSelectionTimeoutMs') ?? 8_000;
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('mongodb.uri');
+        const dbName = configService.get<string>('mongodb.db');
+        const serverSelectionTimeoutMS = configService.get<number>('mongodb.serverSelectionTimeoutMs') ?? 8_000;
         if (!uri) throw new Error('MONGODB_URI environment variable is required');
         return { uri, dbName, serverSelectionTimeoutMS };
       },
